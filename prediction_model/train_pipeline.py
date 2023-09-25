1c512d0e99caa25710dfcd7b3cb83dc6fbde552d
Utkarsh <utkarsh.bhuiit@gmail.com>
2023-09-25 17:13:57 +0530

    Added files

--- prediction_model/train_pipeline.py
+++ prediction_model/train_pipeline.py
@@ -7,7 +7,7 @@ from prediction_model.config import config
 from prediction_model.processing.data_management import load_dataset, save_pipeline
 import prediction_model.processing.preprocessors as pp
 import prediction_model.pipeline as pl
-from prediction_model.predict import _make_prediction
+from prediction_model.predict import make_prediction
 
 def run_training():
     '''Train the model'''
--- test_.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-@pytest.fixture
-def xyz_func():
-    return "ABC"
